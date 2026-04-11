3458300b3a4380654060c4a81b1a2f9e207b7038
rizki9991 <muhamadrizki9991@gmail.com>
2026-04-11 16:42:17 +0700

    Membuat Api dan test fungtion Login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            if (!Auth::guard('web')->attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'message'=> 'Unauthorized',
+                    'data'=> null
+                ],401);
+            }
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+                    'message' => 'Login',
+                    'data' => [
+                        'token' => $token,
+                        'user' => new UserResource($user)
+                        ]
+                    ], 200);
+
+        } catch (Exception $e) {
+                return response()->json([
+                    'message'=> 'Terjadi Kesalahan',
+                    'error' => $e->getMessage()
+                ],500);
+        }
+    }
+
+    public function me()
+    {
+        try {
+            $user = Auth::user();
+            return response()->json([
+                'message' => 'Profile User Berhasil Diambil',
+                'data' => new UserResource($user)
+                ], 200);
+        } catch (Exception $e) {
+                return response()->json([
+                    'message'=> 'Terjadi Kesalahan',
+                    'error' => $e->getMessage()
+                ],500);
+        }
+    }
+    public function logout()
+    {
+        try {
+            $user = Auth::user();
+            $user->currentAccessToken()->delete();
+            return response()->json([ 
+                'message' => 'Logout Berhasil',
+                'data' => null
+                ], 200);
+        } catch (Exception $e) {
+                return response()->json([
+                    'message'=> 'Terjadi Kesalahan',
+                    'error' => $e->getMessage()
+                ],500);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']); 
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
